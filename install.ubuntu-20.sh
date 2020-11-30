b1920848df313006e39d9d10bfab7ec3b24e70af
uta8a <noumodulity@gmail.com>
2020-11-30 11:43:28 +0900

    fix ubuntu initialize shell script

--- install.ubuntu-20.sh
+++ install.ubuntu-20.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eux
 
-sudo apt -y update && sudo apt -y full-upgrade && sudo apt -y install --no-install-recommends build-essential python3-dev python3-pip python3-venv python-is-python3 libssl-dev libffi-dev git ansible
+sudo apt -y update && sudo apt -y full-upgrade && sudo apt -y install --no-install-recommends build-essential python3-dev python3-pip python3-venv python-is-python3 libssl-dev libffi-dev git ansible curl
